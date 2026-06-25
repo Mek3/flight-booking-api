d41ffec1a8c9f8466fb7d806be6b0bcaf8ea1ec2
Mohammed <mek3>
2026-06-25 14:06:08 +0200

    style(reservations): remove unused PatchMapping import

--- src/main/java/com/aerolinea/flight_booking_api/controllers/ReservationController.java
+++ src/main/java/com/aerolinea/flight_booking_api/controllers/ReservationController.java
@@ -13,7 +13,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
