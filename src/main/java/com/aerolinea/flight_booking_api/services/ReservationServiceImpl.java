471550dc6b66ca51224eea51cdd261b291bdb8b0
Mohammed <mek3>
2026-07-06 18:20:16 +0200

    Revert "fix(reservation): mutate flight state prior to saving to trigger optimistic lock"
    This reverts commit 9507a9fe348493fd093b1bf9dd325ddd3c09fcca.

--- src/main/java/com/aerolinea/flight_booking_api/services/ReservationServiceImpl.java
+++ src/main/java/com/aerolinea/flight_booking_api/services/ReservationServiceImpl.java
@@ -75,8 +75,6 @@ public class ReservationServiceImpl implements ReservationService {
                     String.format(ErrorCode.NOT_ENOUGH_SEATS.getMessage(), reservationRequest.flightId()));
         }
 
-        flight.decreaseAvailableSeats(reservationRequest.numberOfPassengers());
-
         Reservation reservation = Reservation.builder()
                 .reservationCode(UUID.randomUUID().toString().substring(0, 8).toUpperCase())
                 .status(ReservationStatus.PENDING)
