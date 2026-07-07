c1aebb0d1016ef2fc4467314f83e44c9fefd6c70
Mohammed <mek3>
2026-07-07 17:54:31 +0200

    test(security): assert RBAC allows ADMIN and blocks USER on protected routes

--- src/test/java/com/aerolinea/flight_booking_api/security/SecurityIntegrationTest.java
+++ src/test/java/com/aerolinea/flight_booking_api/security/SecurityIntegrationTest.java
@@ -5,26 +5,42 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class SecurityIntegrationTest {
+class SecurityIntegrationTest {
 
     @Autowired
     private  MockMvc mockMvc;
 
     @Test
     @DisplayName("Should return 401 unauthorized when no JWT is provided")
-    public void shouldReturnUnauthorizedWhenNoJwtIsProvided() throws Exception {
+    void shouldReturnUnauthorizedWhenNoJwtIsProvided() throws Exception {
         mockMvc.perform(post("/api/v1/reservations"))
                 .andExpect(status().isUnauthorized());
     }
-    
+
+    @Test
+    @DisplayName("Should return 403 forbiden when a USER role tries to access an ADMIN endpoint")
+    @WithMockUser(roles = "USER")
+    void shouldReturnForbiddenWhenUserRoleTriesToAccessAdminEndpoint() throws Exception {
+      mockMvc.perform(delete("/api/v1/flights/1"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @DisplayName("Should allow access and return 404 Not Found when an ADMIN tries to delete a non-existent flight")
+    @WithMockUser(roles = "ADMIN")
+    void shouldReturnNotFoundWhenAdminRoleAccessesNonExistentAdminEndpoint() throws Exception {
+      mockMvc.perform(delete("/api/v1/flights/1"))
+                .andExpect(status().isNotFound());
+    }
 
 }
