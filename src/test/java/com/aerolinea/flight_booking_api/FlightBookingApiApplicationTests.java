2a8e7048a30098b8bb0307e6ba21d43bd9cc7613
Mohammed <mek3>
2026-07-08 21:27:55 +0200

    test: extend AbstractIntegrationTest and add database cleanup

--- src/test/java/com/aerolinea/flight_booking_api/FlightBookingApiApplicationTests.java
+++ src/test/java/com/aerolinea/flight_booking_api/FlightBookingApiApplicationTests.java
@@ -3,11 +3,14 @@ package com.aerolinea.flight_booking_api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.aerolinea.flight_booking_api.config.AbstractIntegrationTest;
+
 @SpringBootTest
-class FlightBookingApiApplicationTests {
+class FlightBookingApiApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
 	}
+
 
 }
