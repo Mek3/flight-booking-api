d862c2d6dd4df92db5190fac61f7bc9c9c9f80a7
Mohammed <mek3>
2026-09-20 20:28:09 +0200

    refactor: remove ReservationDTO, ReservationMapper and ReservationRequest

--- src/main/java/com/aerolinea/flight_booking_api/dtos/ReservationRequest.java
+++
@@ -1,8 +0,0 @@
-package com.aerolinea.flight_booking_api.dtos;
-
-import jakarta.validation.constraints.NotNull;
-
-public record ReservationRequest(
-        @NotNull Long flightInstanceId,
-        @NotNull Integer numberOfPassengers
-) {}
